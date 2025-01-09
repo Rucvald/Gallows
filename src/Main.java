0bc8fe70d3512d151137ea62272960911ed8f45e
Rucvald <p.rucvald@gmail.com>
2025-01-09 12:44:49 +0300

    Main class fixes

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,53 @@
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
-
-        File file = new File("D:/Programs/Lessons/Gallows", "words.txt");
+    public static void main(String[] args) throws IOException {
         menu();
     }
 
     public static void menu() throws FileNotFoundException {
         boolean play = true;
         System.out.println("Welcome to Gallows!");
-        System.out.println("Are you want to play or want to exit?\nIf you want to play, enter 'play' or 'exit' to exit'");
-        while(play){
+        while(play) {
+            System.out.println("If you want to play, enter 'play' or 'exit' to exit:");
             Scanner scanner = new Scanner(System.in);
             String console = scanner.nextLine();
 
-            if(console.equals("play")){
-                Interface interface1 = new Interface();
-                interface1.interfaceMethod();
-            }
-            if(console.equals("exit")){
-                System.out.println("Goodbye!");
-                play = false;
-            }
-            else
-                System.out.println("Wrong input! If you want to play, enter 'play' or 'exit' to exit'");
-        }
+            ChoiceToGame choice = ChoiceToGame.DEFAULT;
 
+            switch(console) {
+                case "play":
+                    choice = ChoiceToGame.PLAY;
+                    break;
+                case "exit":
+                    choice = ChoiceToGame.EXIT;
+                    break;
+                default:
+                    System.out.println("Wrong input!");
+                    break;
+            }
+
+            switch(choice) {
+                case PLAY:
+                    Interface gameInterface = new Interface();
+                    gameInterface.interfaceMethod();
+                    break;
+                case EXIT:
+                    System.out.println("Goodbye!");
+                    play = false;
+                    break;
+            }
+        }
+    }
+}
+enum ChoiceToGame {
+    DEFAULT(" "),
+    PLAY("play"),
+    EXIT("exit");
+    private String choice;
+    ChoiceToGame(String choice) {
+        this.choice = choice;
     }
 }
